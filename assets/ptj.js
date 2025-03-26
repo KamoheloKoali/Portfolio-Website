3c6c204c82bbcc621bd7378c15b4e151b5575f7e
Kamohelo Koali <kamohelokoali201@gmail.com>
2025-03-26 08:26:00 +0200

    remove ship swift, update image for suzannescareersolutions and remove swiper

--- assets/ptj.js
+++ assets/ptj.js
@@ -86,19 +86,19 @@ modalCloses.forEach((modalClose) => {
 });
 
 /*======================= Portfolio Swiper ===================*/
-var swiper = new Swiper(".portfolio__container", {
-  cssMode: true,
-  loop: true,
+// var swiper = new Swiper(".portfolio__container", {
+//   cssMode: true,
+//   loop: true,
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll("section[id]");
